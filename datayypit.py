9a427e65e3ba3d885954450686add340d963e4a6
mpan <Matti.Panula@seamk.fi>
2026-01-16 20:14:51 +0200

    datatyypit

---
+++ datayypit.py
@@ -0,0 +1,8 @@
+merkkijono = "moi"
+kokonaisluku = 3
+liukuluku = 1/3
+totuusarvo = True
+print(merkkijono, type(merkkijono)) # moi <class 'str'>
+print(kokonaisluku, type(kokonaisluku)) # 3 <class 'int'>
+print(liukuluku, type(liukuluku))   # 0.3333333333333333 <class 'float'>
+print(totuusarvo, type(totuusarvo)) # True <class 'bool'>
